f009d10d3e400b80a16d788ee821ec242a4a817b
ABarboz4 <armando.barbozaojeda4@t-mobile.com>
2021-07-27 15:14:50 -0400

    Adding unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ CommonModule ]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  beforeAll( ()=> {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    fixture.detectChanges();
+    app = fixture.componentInstance;
+  });
+
+  it('should create the app', () => {
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'pa-starter-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('pa-starter-angular');
   });
 
@@ -26,6 +31,18 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pa-starter-angular app is running!');
+    //I need to change this because it was failing
+    expect(compiled.querySelector('.doNotRemoveMe').textContent).toContain('Hello world.');
   });
+
+  it('should call onSubmit function with empty values', () => {
+    app.onSubmit('', '');
+    expect(app.isFormHasErrors).toBe(true);
+  });
+
+  it('should call onSubmit function with valid data', () => {
+    app.onSubmit('Recipe 1', 'Recipe 2 Instructions');
+    expect(app.isFormVisible).toBe(false);
+    expect(app.recipes.length).toBe(1);
+  })
 });
