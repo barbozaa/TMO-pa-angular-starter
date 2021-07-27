04429e1575339e4e27f64280682e811a6e56c47f
ABarboz4 <armando.barbozaojeda4@t-mobile.com>
2021-07-27 14:53:58 -0400

    Recipes tmo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface IRecipe {
+  name: string;
+  instructions: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pa-starter-angular';
+  recipes: Array<IRecipe> = [];
+  isFormVisible = false;
+  isFormHasErrors = false;
+
+  onSubmit(name: string, instructions: string): void {
+    if (name && instructions) {
+      this.recipes.unshift({ name, instructions });
+      this.isFormVisible = false;
+    } else {
+      this.isFormHasErrors = true;
+    }
+  }
 }
